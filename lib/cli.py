7e1946622976798e80add4356309613ec7425041
Keith <keith.githinji@student.moringaschool.com>
2024-12-16 11:33:27 +0300

    [200~Implement user prompts for first and second city names and their countries

--- lib/cli.py
+++ lib/cli.py
@@ -9,3 +9,15 @@ plane_emoji = "🛬"
 
 # Display a welcome message
 print(blue('Welcome to the distance calculator!\n'))
+
+# Use inquirer to prompt the user for the first city's name and country
+first_city = inquirer.prompt([
+    inquirer.Text('name', message='Enter the name of the first city: '),
+    inquirer.Text('country', message='Enter the name of the first country: ')
+])
+
+# Prompt the user for the second city's name and country
+second_city = inquirer.prompt([
+    inquirer.Text('name', message='Enter the name of the second city: '),
+    inquirer.Text('country', message='Enter the name of the second country: ')
+])
