d4449c6234fda47fcc8da6f8f1640a0aa2f300e0
Keith <keith.githinji@student.moringaschool.com>
2024-12-16 11:20:52 +0300

    Added a display welcome message

--- lib/cli.py
+++ lib/cli.py
@@ -1,3 +1,11 @@
 import inquirer
 from geopy import distance, geocoders
 from simple_chalk import green, blue, yellow
+
+# Define emojis for different modes
+walking_emoji = "🚶"
+bus_emoji = "🚌"
+plane_emoji = "🛬"
+
+# Display a welcome message
+print(blue('Welcome to the distance calculator!\n'))
